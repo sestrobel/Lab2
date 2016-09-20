7102ce27a7522ccdfbe27e73f986c4ebb7285473
Elizabeth <Elizabeth@wifi-roaming-128-4-99-238.host.udel.edu>
2016-09-20 14:39:24 -0400

    initial commit

---
+++ Lab2Updated/src/test/java/pokerBase/CardTest.java
@@ -0,0 +1,45 @@
+package pokerBase;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class CardTest {
+	static Card x; 
+	//create var
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		//x = new Card()
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+		//
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+		//use vars from beforeclass
+	}
+	
+	@Test
+	public void test2() {
+		fail("Not yet implemented");
+		//use vars from beforeclass
+	}
+	
+
+}
